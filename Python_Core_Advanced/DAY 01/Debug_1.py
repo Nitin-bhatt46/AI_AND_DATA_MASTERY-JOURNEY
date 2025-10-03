ae508d8b23eeb7963d42a38f71aa384b7e725bdc
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2025-10-03 10:27:53 +0530

    Create Debug_1.py

---
+++ Python_Core_Advanced/DAY 01/Debug_1.py
@@ -0,0 +1,5 @@
+# fixing the code 
+print("After fixing the code")
+city = input("Enter your city: ") 
+year = input("Enter your birth year: ") 
+print("You are from " + city +" "+ year + "!") # concatinating space in betweem
