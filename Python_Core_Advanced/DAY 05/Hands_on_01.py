19e3403aa577094293e4e453ccd2506b7d235e19
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2025-10-09 10:13:05 +0530

    Create Hands_on_01.py

---
+++ Python_Core_Advanced/DAY 05/Hands_on_01.py
@@ -0,0 +1,33 @@
+#Exploration With Functions.
+# tiny functions tip + username
+
+def get_username(first, year):
+    return first.strip() + "_" + str(year)
+
+def calculate_total(bill, tip_percent):
+    return bill + (bill * tip_percent / 100)
+
+def split_amount(total, people):
+    return total / people
+
+def main():
+    first = input("First name: ")
+    year = int(input("Birth year: "))
+    bill = float(input("Bill amount: "))
+    tip = float(input("Tip %: "))
+    people = int(input("Number of people: "))
+
+    if people >= 1:
+        username = get_username(first, year)
+        total = calculate_total(bill, tip)
+        per = split_amount(total, people)
+    else:
+        print("Please enter a number between 1 and 100")
+
+
+    print(f"Username: {username}")
+    print(f"Total: {total:.2f}")
+    print(f"Each pays: {per:.2f}")
+
+if __name__ == "__main__":
+    main()
