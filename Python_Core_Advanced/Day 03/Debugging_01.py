77a4c527defdafaf903b05e6bf9f0141c3db3d7e
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2025-10-09 10:11:02 +0530

    Delete Python_Core_Advanced/Day 03 directory

--- Python_Core_Advanced/Day 03/Debugging_01.py
+++
@@ -1,9 +0,0 @@
-# Handling special case where Rock beats Scissors  
-
-elif user_input == 0 and computer_input == 2:  
-print(game_image[user_input])  
-user+=1   print("You WIN!")  
-elif user_input == 2 and computer_input == 0:  
-print(game_image[user_input])  
-computer+=1  
-print("You lose!")
--- Python_Core_Advanced/Day 03/Debugging_02.py
+++
@@ -1,4 +0,0 @@
-if user_input>=0 and user_input<3:
- 	print("You chose", game_image[user_input])
-else:
- 	print("invalid input"
