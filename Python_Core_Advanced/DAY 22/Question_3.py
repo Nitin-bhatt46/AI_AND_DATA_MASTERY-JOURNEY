d96fdd10ebf73b6b97937caf639f6deb8e64c2fc
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2025-11-04 15:39:28 +0530

    Create Question_3.py

---
+++ Python_Core_Advanced/DAY 22/Question_3.py
@@ -0,0 +1,14 @@
+from  turtle import Turtle, Screen
+
+def star():
+    for _ in range(5):
+        our_turtle.forward(100)
+        our_turtle.right(144)
+
+our_turtle = Turtle()
+
+our_turtle.shape("turtle")
+
+our_turtle.color("red","blue")
+
+star()
