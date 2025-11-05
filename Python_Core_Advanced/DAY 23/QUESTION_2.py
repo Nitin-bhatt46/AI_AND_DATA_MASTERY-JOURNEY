0db008b0c031de8bde2f936be5ae09b1c642b2fa
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2025-11-05 16:38:11 +0530

    Create QUESTION_2.py

---
+++ Python_Core_Advanced/DAY 23/QUESTION_2.py
@@ -0,0 +1,46 @@
+# simple way 
+# longest way 
+from  turtle import Turtle, Screen
+
+
+t = Turtle()
+
+t.shape("arrow")
+
+t.color("red","blue")
+
+for i in range(3):
+    t.left(120)
+    t.forward(100)
+
+for i in range(4):
+    t.left(90)
+    t.forward(100)
+
+
+for i in range(5):
+    t.left(72)
+    t.forward(100)
+
+for i in range(6):
+    t.left(60)
+    t.forward(100)
+
+
+for _ in range(7):
+    t.left(51.42)
+    t.forward(100)
+
+
+for _ in range(8):
+    t.left(45)
+    t.forward(100)
+
+
+for _ in range(9):
+    t.left(40)
+    t.forward(100)
+
+for _ in range(10):
+    t.left(36)
+    t.forward(100)
